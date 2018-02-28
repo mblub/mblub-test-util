6bef4687ff99e916d84b7db2d9abb84795b0a8b0
Mike Blubaugh <mblubaugh@gmail.com>
2018-02-28 10:25:59 -0800

    assertions for optionals; deprecate StreamTestingUtils

--- src/main/java/com/mblub/unit/Assertions.java
+++ src/main/java/com/mblub/unit/Assertions.java
@@ -1,9 +1,17 @@
 package com.mblub.unit;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 import java.util.Iterator;
+import java.util.Optional;
 import java.util.stream.Stream;
 
-import org.junit.Assert;
+import org.apache.commons.lang3.StringUtils;
+import org.hamcrest.Matcher;
 
 public class Assertions {
   public static <T> void assertStreamContents(Stream<T> expectStream, Stream<T> actualStream) {
@@ -18,14 +26,53 @@ public class Assertions {
     assertIteratorContents(null, expectIter, actualIter);
   }
 
+  private static String getContextPrefix(String context) {
+    return StringUtils.isEmpty(context) ? "" : context + ": ";
+  }
+
   public static <T> void assertIteratorContents(String context, Iterator<T> expectIter, Iterator<T> actualIter) {
+    String contextPrefix = getContextPrefix(context);
     int itemIdx = 0;
     while (expectIter.hasNext() && actualIter.hasNext()) {
-      Assert.assertEquals((context == null ? "" : context + ": ") + "item " + itemIdx + " differs", expectIter.next(),
-              actualIter.next());
+      assertEquals(contextPrefix + "item " + itemIdx + " differs", expectIter.next(), actualIter.next());
       itemIdx += 1;
     }
-    Assert.assertFalse("actual has " + itemIdx + " entries, but expected more", expectIter.hasNext());
-    Assert.assertFalse("expected " + itemIdx + " entries, but actual had more", actualIter.hasNext());
+    assertFalse(contextPrefix + "actual has " + itemIdx + " entries, but expected more", expectIter.hasNext());
+    assertFalse(contextPrefix + "expected " + itemIdx + " entries, but actual had more", actualIter.hasNext());
+  }
+
+  public static <T> void assertOptionalPresent(String context, Optional<T> candidate) {
+    assertTrue(getContextPrefix(context) + "not present", candidate.isPresent());
+  }
+
+  public static <T> void assertOptionalSame(String context, T expectValue, Optional<T> actualOptional) {
+    assertOptionalPresent(context, actualOptional);
+    assertSame(context, expectValue, actualOptional.get());
+  }
+
+  public static <T> void assertOptionalSame(String context, Optional<T> expectOptional, Optional<T> actualOptional) {
+    assertEquals(getContextPrefix(context) + "presence of optional", expectOptional.isPresent(),
+            actualOptional.isPresent());
+    if (expectOptional.isPresent()) {
+      assertSame(context, expectOptional.get(), actualOptional.get());
+    }
+  }
+
+  public static <T> void assertOptionalEquals(String context, T expectValue, Optional<T> actualOptional) {
+    assertOptionalPresent(context, actualOptional);
+    assertEquals(context, expectValue, actualOptional.get());
+  }
+
+  public static <T> void assertOptionalEquals(String context, Optional<T> expectOptional, Optional<T> actualOptional) {
+    assertEquals(getContextPrefix(context) + "presence of optional", expectOptional.isPresent(),
+            actualOptional.isPresent());
+    if (expectOptional.isPresent()) {
+      assertEquals(context, expectOptional.get(), actualOptional.get());
+    }
+  }
+
+  public static <T> void assertOptionalThat(String context, Optional<T> candidate, Matcher<? super T> matcher) {
+    assertOptionalPresent(context, candidate);
+    assertThat(context, candidate.get(), matcher);
   }
 }
--- src/main/java/com/mblub/unit/StreamTestingUtil.java
+++ src/main/java/com/mblub/unit/StreamTestingUtil.java
@@ -7,19 +7,31 @@ import static org.junit.Assert.assertTrue;
 import java.util.Iterator;
 import java.util.stream.Stream;
 
+/**
+ * @deprecated use Assertions.assertStreamContents instead
+ * @author mike
+ *
+ */
+@Deprecated
 public class StreamTestingUtil {
 
-    public static <T> void assertStreamsEqual(String messageHeader, Stream<T> expectStream, Stream<T> actualStream) {
-        Iterator<T> expectIter = expectStream.iterator();
-        Iterator<T> actualIter = actualStream.iterator();
-        int idx = 0;
-        while (expectIter.hasNext()) {
-            assertTrue(messageHeader + ": actual stream has " + (idx + 1) + " elements, but expected stream has more",
-                actualIter.hasNext());
-            assertEquals(messageHeader + ": element idx " + idx, expectIter.next(), actualIter.next());
-            idx += 1;
-        }
-        assertFalse(messageHeader + ": expected stream has " + (idx + 1) + " elements, but actual stream has more",
-            actualIter.hasNext());
+  /**
+   * @deprecated use Assertions.assertStreamContents instead
+   * @author mike
+   *
+   */
+  @Deprecated
+  public static <T> void assertStreamsEqual(String messageHeader, Stream<T> expectStream, Stream<T> actualStream) {
+    Iterator<T> expectIter = expectStream.iterator();
+    Iterator<T> actualIter = actualStream.iterator();
+    int idx = 0;
+    while (expectIter.hasNext()) {
+      assertTrue(messageHeader + ": actual stream has " + (idx + 1) + " elements, but expected stream has more",
+              actualIter.hasNext());
+      assertEquals(messageHeader + ": element idx " + idx, expectIter.next(), actualIter.next());
+      idx += 1;
     }
+    assertFalse(messageHeader + ": expected stream has " + (idx + 1) + " elements, but actual stream has more",
+            actualIter.hasNext());
+  }
 }
